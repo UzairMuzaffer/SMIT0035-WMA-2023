c81a9075577302db29d6d1e0467987400538daad
UzairMuzaffer <uzairmuzaffer67@gmail.com>
2023-08-15 11:53:02 +0500

    first commit

---
+++ javascript/class 1/index.js
@@ -0,0 +1,8 @@
+// var ko name dete hein
+// name koi reserve keyword nhi ho sakta
+// name kisi (alphabet $ _) se start ho sakta he is k ilawa kisi or se start nhi ho sakta
+// name k bich or akhir me (numbers $ _) aa sakte hein
+// var ki value text(string) ho sakti he
+
+var student = 'laptop';
+alert(student);
---
+++ javascript/class 2/index.js
@@ -0,0 +1,36 @@
+/*
+    values for variables
+    1. String("", '')
+    2. Number
+    craetion of variable
+    assignment of variable
+    reassign of variable
+    BODMAS
+*/
+
+
+/*
+var jug = 'water';
+jug = 'milk';
+jug = 'juice';
+jug = 'rohafza';
+alert(jug);
+*/
+
+// var studentName = 'Asif     ';
+// var studentId = 400;
+// var studentIncom = studentId * 10;
+// var studentTax = (2 + studentIncom) / 100 * 2;
+// console.log(studentTax);
+// console.log(studentIncom);
+// alert(studentName + studentId);
+
+// increament
+// var counter = 10;
+// counter = counter + 1;
+// counter = counter + 1;
+
+var counter = 10;
+counter++;
+counter++;
+console.log(counter);
