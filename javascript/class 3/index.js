bba79e0d1785b62dcfa8a0c47be7e9c181c1cda5
UzairMuzaffer <uzairmuzaffer67@gmail.com>
2023-08-17 10:56:09 +0500

    class 3 chapter 6 to 9 compeleted

---
+++ javascript/class 3/index.js
@@ -0,0 +1,71 @@
+/*
+ 1. post increament
+ 2. pre increament
+ 3. post decreament
+ 4. pre decreament
+ 5. Concatenating
+ 6. prompt
+*/
+
+/*
+var counter = 10;
+counter = counter + 1; // 10 + 1 = 11
+counter = counter + 1; // 11 + 1 = 12
+*/
+
+/*
+//! post increament
+var counter = 10;
+var newValue =  counter++
+// post increament step 1: updation (counter = counter + 1)
+// post increament step 2: (retutn = 10) old value
+console.log(counter)
+console.log(newValue)
+*/
+
+/*
+//! pre increament
+var counter = 10;
+var newValue =  ++counter
+// pre increament step 1: updation (counter = counter + 1)
+// pre increament step 2: (retutn = 11) updated value
+console.log(counter)
+console.log(newValue)
+*/
+
+/*
+//! post decrement
+var counter = 10;
+var newValue =  counter-- // 10 - 1 = 9
+// post decrement step 1: updation (counter = counter - 1)
+// post decrement step 2: (retutn = 10) old value
+console.log(counter)
+console.log(newValue)
+*/
+
+/*
+// pre decrement
+var counter = 10;
+var newValue =  --counter // 10 - 1 = 9
+// pre decrement step 1: updation (counter = counter - 1)
+// pre decrement step 2: (retutn = 9) updated value
+console.log(counter)
+console.log(newValue)
+*/
+
+//! Concatenating
+// var greet = "Hello from class ";
+// var classNumber = 27;
+// var newString = greet + classNumber;
+// var plus = 10 + '10'
+// console.log(plus)
+// console.log(newString)
+// var studentId = 83823;
+// var studentName = 'Noman';
+// var isStudentAvailable = 'No';
+
+// console.log('student Id: ' + studentId + " " + "Student name: " + studentName + " isStudentAvailable: " + isStudentAvailable);
+
+//! prompt
+var studentName = prompt('Tell me your name');
+console.log(studentName);
