8169de8b3986cd4ae7ec9bcd64d3d685e1c51bc1
UzairMuzaffer <uzairmuzaffer67@gmail.com>
2023-09-01 11:33:40 +0500

    class 9 for loop

---
+++ javascript/class 9/index.js
@@ -0,0 +1,63 @@
+/*
+  1. For Loop
+      step 1: variable initialization
+      step 2: if condition true (then run block of code)
+              else do not run step 3 and break loop
+      step 3: run block of code
+      step 4: update variable
+
+  2. break keyword:
+  3. Continue keyword:
+  4. loop on array
+
+  question :
+   1: if condition true then loop run
+   2: if condition false then loop break
+   3: pre & post increment decrement both will run
+*/
+
+
+//! increment
+// for (var i = 1; i <= 5; i++) {
+//   console.log(i);
+// }
+
+//! decrement
+// for (var i = 5; i >= 1; --i) {
+//   console.log(i);
+// }
+
+
+//! break
+// for (var i = 1; i <= 5; i++) {
+//   if (i === 3) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+//! continue
+// for (var i = 1; i <= 5; i++) {
+//   console.log(1);
+//   if (i === 3) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+//! Loop on Array
+
+// var arr = ['a', 'b', 'c'];
+// console.log(arr)
+// // console.log(arr.length);
+// // console.log(arr[0]);
+// // console.log(arr[1]);
+// // console.log(arr[2]);
+
+// for (var i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+//   if(i === 2){
+//     arr[2] = 'z'
+//   }
+// }
+// console.log(arr);
