9c2a70e140384fc0bcbce8444efeb84129df467a
UzairMuzaffer <uzairmuzaffer67@gmail.com>
2023-09-15 16:31:08 +0500

    class 15

---
+++ javascript/class 15/index.js
@@ -0,0 +1,33 @@
+/*
+  html manipulation
+  1. querySelector
+  2. h1.innerText
+  3. reassigning innerText
+  4. h1.innerHTML
+  5. reassigning innerHTML
+  6. input.value
+  7. input.valueAsNumber
+
+  8. querySelectorAll
+*/
+
+// var h1 = document.querySelector('h1');
+// console.log(h1.innerText);
+// h1.innerText += "Class 15 Hello";
+// console.log(h1.innerHTML);
+// h1.innerHTML = `Class 15 <span> Hello </span>`;
+
+// var myElement = document.querySelector('.myClass');
+// console.log(myElement);
+
+// var inp = document.querySelector('.inp');
+// console.log(inp.valueAsNumber);
+
+// var allH1 = document.querySelectorAll('h1');
+
+// for (let i = 0; i < allH1.length; i++) {
+//   allH1[i].innerText = "ok";
+// }
+
+// create this challenge
+//https://www.frontendmentor.io/challenges/interactive-comments-section-iG1RugEG9 
