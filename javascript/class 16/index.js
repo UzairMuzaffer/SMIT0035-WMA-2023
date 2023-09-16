31e19bcad2068b98fa7edaf2ac4255ee189f3a05
UzairMuzaffer <uzairmuzaffer67@gmail.com>
2023-09-16 16:15:04 +0500

    class 16 all about Date

---
+++ javascript/class 16/index.js
@@ -0,0 +1,13 @@
+/*
+  Date
+  1. Date constructor
+  2. Date constructor call with new keyword | new Date() |
+  3. rightNow.getDate() // month date
+  4. rightNow.getDay() // week day
+  5. rightNow.getFullYear() // full year
+  6. rightNow.getHours() // current hour
+  7. rightNow.getMonth() // month number start from index 0
+  8. all methods of new Date() constructor
+  9. new Date(milliseconds)
+  10. new Date('01-01-2000')
+*/
