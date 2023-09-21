d20d06cc89689425df08e45b68ce8056fc3bbd84
UzairMuzaffer <uzairmuzaffer67@gmail.com>
2023-09-21 11:35:34 +0500

    class 18 functions

---
+++ javascript/class 18/index.js
@@ -0,0 +1,41 @@
+/*
+1. debugger
+  Functions
+  1. function syntax
+        - function keyword
+        - function name
+        - function round brackets
+        - function parameters in round brackets
+          separated by coma
+        - function body or block
+        - function body code
+        - function return something
+
+  2. function call with arguments separated by coma.
+  3. save some value that returning from function.
+*/
+
+// !
+// function add(a, b) {
+//   // debugger;
+//   var addition = a + b;
+//   // debugger;
+//   return addition;
+// }
+
+// var result = add(5, 9);
+// // debugger;
+// console.log(result);
+
+// !
+// function userInfo(firstName, lastName, birthYear) {
+//   var fullName = firstName + " " + lastName;
+//   var dateIns = new Date();
+//   var currentYear = dateIns.getFullYear();
+//   var age = currentYear - birthYear;
+//   debugger;
+//   return [fullName, age];
+// }
+
+// var result = userInfo("ali", "raza", 2000);
+// console.log(result);
