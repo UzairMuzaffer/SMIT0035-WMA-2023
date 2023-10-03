39e224f12ccab828fccc2be8227eb260276fecf8
Uzair Muzaffer ⚡ <uzairmuzaffer67@gmail.com>
2023-10-03 10:33:36 +0500

    class 23 41 to 44 & test(21 to 40)

---
+++ javascript/class 23/index.js
@@ -0,0 +1,38 @@
+/*
+  1. while loop
+  2. do while loop
+  3. placing script
+  4. commenting
+  5. test (21 to 40)
+*/
+
+
+// 1. while loop
+
+// let i = 1
+// while (i <= 10){
+//   console.log(i++)
+// }
+
+// let i = 1
+// let count = 0
+// while (i <= 10){
+//   let isOdd = i % 2 === 0
+//   if(isOdd) count++
+//   i++
+// }
+// console.log(count)
+
+// let sum = 0;
+// let number = parseInt(prompt('Enter a number: '));
+// while(number >= 0) {
+//     sum += number;
+//     number = parseInt(prompt('Enter a number: '));
+// }
+
+// 2.
+// let i = 9
+
+// do {
+//   console.log(++i)  
+// } while(i <= 10)
