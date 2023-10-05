bdb6f6d8c028ea74f379cff46d7cc92f4b9e7d96
UzairMuzaffer <uzairmuzaffer67@gmail.com>
2023-10-05 11:34:33 +0500

    class 24

---
+++ javascript/class 24/index.js
@@ -0,0 +1,37 @@
+/*
+  1. revision (while loop , do while loop)
+  2. for of loop
+  3. onClick
+  4. onmouseover
+  5. onmouseout
+*/
+
+//! 1. while loop
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// while (arr.length > 0) {
+//   arr.pop();
+//   console.log(arr);
+// }
+
+// 2. do while loop
+// let arr = [];
+// do {
+//   if (arr.length === 0) {
+//     arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//   } else {
+//     arr.pop();
+//   }
+//   console.log(arr);
+//   debugger;
+// } while (arr.length > 1);
+
+//! 2. for of loop
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9 ,10];
+
+// for (const element of arr) {
+//   console.log(element);
+// }
+
+function alertMyName(name) {
+  console.log(name);
+}
