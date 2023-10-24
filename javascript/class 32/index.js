0fc2f2718ef2c50f2fc402e3e36d0169c8f92587
UzairMuzaffer <uzairmuzaffer67@gmail.com>
2023-10-24 11:03:24 +0500

    class 32

---
+++ javascript/class 32/index.js
@@ -0,0 +1,82 @@
+
+/*
+  1. Object constructor
+  2. Object.keys()
+  3. Object.values()
+  4. Object.entries()
+*/
+
+//! Q. write a object that have student info & after that Write a method that returns a deep array like [[key, value]] from that object
+
+// const myObj = {
+//   name: 'ali',
+//   age: 28,
+// };
+
+// function convertObjIntoArr(obj) {
+//   return Object.entries(obj);
+// }
+
+// convertObjIntoArr(myObj)
+
+//! Q Write a JavaScript program to create a object called "person" with properties for name, age and country. Include a method to display the person's details.
+
+// const student = {
+//   age: 23,
+//   name: 'ali',
+//   id: '9032esi',
+//   country: 'pakistan',
+//   getStudentInfo: function () {
+//     return `student name is ${this.name} & age is ${this.age}`;
+//   }
+// };
+
+// const info = student.getStudentInfo()
+
+// console.log(info);
+
+//! Q  Write a JavaScript program to create a object called 'rectangle' with properties for width and height.Include two methods to calculate rectangle area and perimeter.
+
+// formula to calculate rectangle area: width * height
+// formula to calculate rectangle perimeter:  (width + height) * 2
+
+// const rectangle = {
+//   height: 20,
+//   width: 20,
+//   getArea: function () {
+//     return this.height * this.width;
+//   },
+//   getPerimeter: function () {
+//     return (this.height + this.width) * 2;
+//   }
+// };
+// const area = rectangle.getArea();
+// const perimeter = rectangle.getPerimeter();
+
+
+
+/*
+
+
+3. Write a JavaScript program that creates a object called 'vehicle' with properties for make, model, and year. Include a method to display vehicle details.
+
+
+4. Write a JavaScript program that creates a object called "bankAccount" with properties for account number and balance. Include methods to deposit and withdraw money from the account.
+
+
+
+6. Write a JavaScript program that creates a object called 'employee' with properties for name and salary. Include a method to calculate annual salary.
+
+7. Write a JavaScript program that creates a object `book` with properties for title, author, and publication year. Include a method to display book details.
+
+
+8. Write a JavaScript program that creates a object called 'animal' with properties for species and sound. Include a method to make the animal's sound.
+
+9. Write a JavaScript program that creates a object called bank with properties for bank names and branches. Include methods to add a branch, remove a branch, and display all branches.
+
+
+10. Write a JavaScript program that creates a object called product with properties for product ID, name, and price. Include a method to calculate the total price by multiplying the price by the quantity.
+
+12. Write a JavaScript program that creates a object called University with properties for university name and departments. Include methods to add a department, remove a department, and display all departments.
+
+*/
