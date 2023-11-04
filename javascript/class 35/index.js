750caf7ea1d611764e88909be1ea2c104dc3fd67
Uzair Muzaffer ⚡ <uzairmuzaffer67@gmail.com>
2023-11-04 10:54:40 +0500

    bump

--- javascript/class 35/index.js
+++ javascript/class 35/index.js
@@ -45,3 +45,32 @@ substract(10,2)
 //     }
 // }
 // obj.getAge()
+
+/*
+ 1. how to create cunstructor with properties & methods
+ 2. how to call cunstructor
+ 3. what new keyword does
+ 4. what is instanse
+*/
+
+function MyHuman(dob, name, gender) {
+   this.dob = dob;
+   this.name = name;
+   this.gender = gender;
+}
+
+MyHuman.prototype.calcAge = function() {
+  return 23 - this.dob
+};
+
+const obj = new MyHuman(2000, 'ali', 'male')
+const obj2 = new MyHuman(2000, 'ali', 'male')
+const obj3 = new MyHuman(2000, 'ali', 'male')
+console.log(obj, obj2, obj3)
+
+/*
+          _  // Object
+    _          _
+  _ _ _ _    _ _ _ _
+
+*/
