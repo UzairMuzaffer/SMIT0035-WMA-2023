b00f160b3d598cdcd47a8baf2ef36050e24bcc5a
UzairMuzaffer <uzairmuzaffer67@gmail.com>
2023-11-07 10:45:05 +0500

    class 37

---
+++ javascript/class 37/index.js
@@ -0,0 +1,27 @@
+
+/*
+  1. prototypal inheritance
+*/
+
+const obj = {};
+const obj1 = new Object();
+
+const arr = [];
+const arr1 = new Array();
+
+
+/*
+  1. prototype
+  2. __proto__
+  3. private methods & properties
+*/
+
+
+/*
+  1. new keyword created plain empty object {} called newInstance
+  2. {}(newInstance) linked with constructor function prototype property
+  3. constructor functions execute(call) with given arguments
+  4. this = newInstance
+  5. properties & methods withing constructor function is assigned to newInstance 
+  6. constructor function only return non-primitive or be default newInstance
+*/
