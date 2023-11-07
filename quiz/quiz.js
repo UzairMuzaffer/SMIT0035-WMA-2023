f68b50d56e458eacb226f94e044f5ecb090b882d
UzairMuzaffer <uzairmuzaffer67@gmail.com>
2023-11-07 13:43:40 +0500

    quiz updated

---
+++ quiz/quiz.js
@@ -0,0 +1,70 @@
+//! Objects
+
+//* Problem 1
+// Create an object 'person' with properties 'name' and 'age'. Then, write a function 'displayPersonInfo' that takes the person object as an argument and displays the name and age.
+
+//* Problem 2
+// Create an array 'students' with objects representing students. Each student should have 'name' and 'score' properties. Write a function 'findTopStudent' that finds and displays the student with the highest score.
+
+//! Arrays
+
+//* Problem 3
+// Write a function 'calculateAverage' that takes an array of numbers and calculates and displays the average value.
+
+//* Problem 4
+// Create an array 'fruits' and sort it in alphabetical order. Then, display the sorted array.
+
+//! Functions
+
+//* Problem 5
+// Write a function 'factorial' that calculates and displays the factorial of a number.
+
+//* Problem 6
+// Create a function 'reverseString' that takes a string as input and displays the reverse of the input string.
+
+//! Constructor Function
+
+//* Problem 7
+// Create a constructor function 'Person' that takes 'name' and 'age' as parameters and creates person objects. Then, create two person objects and display their details.
+
+//! Loops
+
+//* Problem 8
+// Write a loop that displays numbers from 1 to 10.
+
+//* Problem 9
+// Write a function 'sumArrayElements' that calculates and displays the sum of elements in an array of numbers.
+
+//! If-Else Statements
+
+//* Problem 10
+// Write a function 'isEven' that takes a number as input and displays whether the number is even or odd.
+
+//* Problem 11
+// Write a function 'checkVotingEligibility' that takes a person's age as input and displays whether the person is eligible to vote (18 years or older).
+
+//! Date
+
+//* Problem 12
+// Write a function 'displayCurrentDateTime' that displays the current date and time.
+
+//! Numbers
+
+//* Problem 13
+// Write a function 'isPrime' that takes a number as input and displays whether the number is prime or not.
+
+//* Problem 14
+// Write a function 'squareNumbers' that takes an array of numbers, squares each number, and displays the squared values.
+
+//! Strings
+
+//* Problem 15
+// Write a function 'countVowels' that takes a string as input and displays the count of vowels (a, e, i, o, u) in the string.
+
+//! DOM Manipulation
+
+//* Problem 16
+// Create an HTML button with the text "Click Me" and a <div> element. When the button is clicked, change the text of the <div> to "Button Clicked!".
+
+//* Problem 17
+// Create an HTML form with an input field and a submit button. When the form is submitted, display an alert with the text entered in the input field.
