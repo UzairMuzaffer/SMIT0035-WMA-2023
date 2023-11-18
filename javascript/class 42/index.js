d2f83899ea87b981481c09921326c81a0023f9ef
Uzair Muzaffer ⚡ <uzairmuzaffer67@gmail.com>
2023-11-18 10:52:29 +0500

    CLASS 42

---
+++ javascript/class 42/index.js
@@ -0,0 +1,43 @@
+
+/*
+    Array loops method
+  1. forEach  
+  2. map  
+
+*/
+
+
+//! 1. forEach  
+
+// const arr  = new Array('g','g','v')
+
+// function forEachFn(element, index, arr) {
+//     console.log(arr)
+//     console.log(element)
+//     console.log(index)
+// }
+
+// arr.forEach(forEachFn)
+
+
+// const arr  = new Array('g','g','v','v','a')
+// const obj = {}
+
+// function forEachFn(element, index) {
+//     if (obj[element]) obj[element] += 1
+//     else obj[element] = 1
+// }
+
+// arr.forEach(forEachFn)
+// obj
+
+
+//! 1. map
+
+// const arr  = new Array('g','g','v')
+
+// const methodRtrnArr = arr.map((element, index) => {
+//  return  element + index
+// })
+
+// methodRtrnArr
